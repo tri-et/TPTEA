38cedf75544b346c1ed8d7b1b731ffcb018d2e7d
Unknown <tripham178@gmail.com>
2018-08-16 18:57:58 +0700

    create UI app

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,14 +1,35 @@
 <template>
-  <q-page class="flex flex-center">
-    <img alt="Quasar logo" src="~assets/quasar-logo-full.svg">
-  </q-page>
+  <div>home</div>
 </template>
 
 <style>
+.resizeLogo {
+  transform: scale(0.6);
+}
+.light {
+  transform: scale(0.6);
+  position: absolute;
+  top: -47px;
+  right: -53px;
+}
+.bg-home {
+  background-image: url('~assets/homeBannerBg.jpg');
+}
 </style>
 
 <script>
 export default {
   name: 'PageIndex',
+  methods: {
+    show(options) {
+      this.$q.loading.show(options)
+      setTimeout(() => {
+        this.$q.loading.hide()
+      }, 100000)
+    },
+    noMessage() {
+      this.show()
+    },
+  },
 }
 </script>
