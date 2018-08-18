1241ad6e496ab9615c02e4f352b277c4111b20b3
Light-et <light.elitetech@gmail.com>
2018-08-18 11:58:44 +0700

    add popup modal in view home

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,9 +4,22 @@
     <q-carousel-slide img-src="assets/component.jpg">
       <div class="absolute-top-left custom-caption-top">
         <div class="q-display-1">Bring on the fruit</div>
-      <q-btn class="fixed-bottom-left btnlearn" rounded >
-        <div>Learn More</div>
+      <q-btn class="fixed-bottom-left btnlearn" rounded>
+        <q-item
+          v-for="modal in types"
+          :key="modal.label"
+          @click.native="modal.show()"
+        >
+          <q-item-main :label="modal.label" />
+          </q-item>
         </q-btn>
+         <q-modal v-model="basicModal" :content-css="{minWidth: '50vw'}">
+      <div style="padding: 50px">
+        <div class="q-display-1 q-mb-md">Basic Modal</div>
+        <p v-for="n in 25" :key="`a-${n}`">Scroll down to close</p>
+        <q-btn color="primary" @click="basicModal = false" label="Close" />
+      </div>
+    </q-modal>
       </div>
     </q-carousel-slide>
   </q-carousel>
@@ -59,12 +72,12 @@
   margin-bottom: 20px;
   margin-left: 20px;
 }
-.q-display-1{
-font-size: 40px;
+.q-display-1 {
+  font-size: 40px;
 }
-.inner-top{
+.inner-top {
   font-size: 24px;
-  color: cyan
+  color: cyan;
 }
 </style>
 
@@ -72,7 +85,16 @@ font-size: 40px;
 export default {
   data() {
     return {
+      basicModal: false,
       imageSrc: 'assets/component.jpg',
+      types: [
+        {
+          label: 'Learn More',
+          show: () => {
+            this.basicModal = true
+          },
+        },
+      ],
     }
   },
   name: 'PageIndex',
