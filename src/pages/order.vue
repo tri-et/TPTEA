af5cb6e34a719f45319d0dc2050b01b27a855f3c
Light-et <light.elitetech@gmail.com>
2018-08-21 11:48:10 +0700

    commit New COmponent(5)

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -1,64 +1,10 @@
-
 <template>
-<div>
-    <div class="header">
-    <div id="background"></div>
-    <div id="labels">
-        labels here
-    </div>
-     <q-scroll-observable @scroll="userHasScrolled" />
-</div>
-    <div class="content">
-</div>
-</div>
+  <div>Oder</div>
 </template>
 <script>
 export default {
-  name: 'PageOrder',
-  methods: {
-    userHasScrolled(scroll) {
-      var target = scroll.position / 10000
-      if (scroll.direction === 'down') {
-        console.log(scroll.position / 1000)
-        document.getElementById('labels').style.opacity = target
-      } else {
-        document.getElementById('labels').style.display = 'none'
-      }
-    },
-  },
+  name: 'PageMember',
 }
 </script>
 <style>
-.header {
-  width: 100%;
-  height: 100px;
-  position: fixed;
-  top: 0px;
-  z-index: 3;
-}
-body {
-  margin: 0px;
-}
-.header #background,
-.header #labels {
-  position: absolute;
-  top: 0px;
-  width: 100%;
-  height: 100%;
-}
-
-.header #labels {
-  background-color: transperent;
-}
-
-.header #background {
-  background-color: yellow;
-  display: none;
-}
-
-.content {
-  width: 100%;
-  height: 5000px;
-  background-color: green;
-}
 </style>
