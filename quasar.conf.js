f4cc6c5372da692bc7020751ea146b0c2a484541
hemlk <truonglehiep@gmail.com>
2018-08-21 11:38:54 +0100

    fix abit darker

--- quasar.conf.js
+++ quasar.conf.js
@@ -105,7 +105,7 @@ module.exports = function(ctx) {
         description: 'TP-Tea. The Best Tea in town!',
         display: 'standalone',
         orientation: 'portrait',
-        background_color: '#BFBDAEdeploy',
+        background_color: '#BFBDAE',
         theme_color: '#027be3',
         icons: [
           {
