6a769e1d8adfeaf420dac1c9a6d505ba06b735a2
Phillip <phillip7.et@gmail.com>
2018-08-22 23:19:58 +0700

    add routing 3 form login, member, register, control by two variables isReq & isAuth
    add more member form temporarily too

--- src/pages/member.vue
+++ src/pages/member.vue
@@ -5,8 +5,10 @@
   </div>
 </template>
 <script>
+import {mapGetters} from 'vuex'
 import formLogin from '../components/login.vue'
 import formRegister from '../components/register.vue'
+import formMember from '../components/member.vue'
 export default {
   name: 'PageMember',
   components: {
@@ -18,7 +20,20 @@ export default {
       currentComponent: formLogin,
     }
   },
-  computed: {},
+  computed: {
+    ...mapGetters('member', ['getIsReq', 'getIsAuth']),
+  },
+  created() {
+    if (this.getIsAuth) {
+      this.currentComponent = formMember
+    } else {
+      if (this.getIsReq) {
+        this.currentComponent = formRegister
+      } else {
+        this.currentComponent = formLogin
+      }
+    }
+  },
 }
 </script>
 <style>
