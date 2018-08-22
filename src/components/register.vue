a33a37841f7ca1bf2f460c062c255fbf13db77a0
Phillip <phillip7.et@gmail.com>
2018-08-22 23:02:42 +0700

    add login and register form
    import to member, and set default form is login form

---
+++ src/components/register.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Register</h1>
+  </div>
+</template>
+<script>
+export default {
+  name: 'RegisterFrom',
+}
+</script>
+<style>
+</style>
--- src/pages/member.vue
+++ src/pages/member.vue
@@ -1,9 +1,24 @@
 <template>
-  <div>Member</div>
+  <div>
+    <component v-bind:is="currentComponent"></component>
+
+  </div>
 </template>
 <script>
+import formLogin from '../components/login.vue'
+import formRegister from '../components/register.vue'
 export default {
   name: 'PageMember',
+  components: {
+    'form-login': formLogin,
+    'form-register': formRegister,
+  },
+  data() {
+    return {
+      currentComponent: formLogin,
+    }
+  },
+  computed: {},
 }
 </script>
 <style>
