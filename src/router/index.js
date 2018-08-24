277f17c9d0652168c2f5aa5d451111245a277ee3
Phillip <phillip7.et@gmail.com>
2018-08-24 15:20:47 +0700

    change algorithm routing, removed isReq variable

--- src/router/index.js
+++ src/router/index.js
@@ -21,25 +21,24 @@ export default function(/* { store, ssrContext } */) {
     base: process.env.VUE_ROUTER_BASE,
   })
   localStorage.setItem('isReq', 'false')
-  let isReq = false // () => localStorage.getItem('isReq')
-  let isAuth = false // () => !!localStorage.getItem('auth-token')
+  let isAuth = true // () => !!localStorage.getItem('auth-token')
 
   Router.beforeEach((to, _, next) => {
     if (isAuth) {
       if (to.path === '/member') {
         next()
+        // prevent route to register page
       } else if (to.path === '/member/register') {
         next('/member')
       } else next()
     } else {
-      if (isReq && !isAuth) {
-        if (to.path === '/member/register' && isAuth === false) next()
-        else next('/member/register')
-      } else {
-        if (to.path === '/member/login') next()
-        else if (top !== '/member/login' && to.path !== '/member') next()
-        else next('/member/login')
-      }
+      if (
+        to.path === '/member/register' ||
+        to.path === '/member/login' ||
+        (to.path !== '/member/login' && to.path !== '/member') // prevent route to member page
+      ) {
+        next()
+      } else next('/member/login')
     }
   })
   return Router
