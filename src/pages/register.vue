aaf0c972994a26548ab3621af9b4fe1141d453bf
Phillip <phillip7.et@gmail.com>
2018-08-27 20:39:29 +0700

    add register page

--- src/pages/register.vue
+++ src/pages/register.vue
@@ -1,12 +1,46 @@
 <template>
-  <div>
-    <h1>Register</h1>
-  </div>
+  <q-page padding class="docs-input row justify-center">
+    <div style="width: 500px; max-width: 90vw;">
+      <q-input v-model="form.username" @blur="$v.form.username.$touch" :error="$v.form.username.$error" float-label="User name" />
+      <q-input v-model="form.password" @blur="$v.form.password.$touch" :error="$v.form.password.$error" float-label="Password" />
+      <q-input v-model="form.email" @blur="$v.form.email.$touch" @keyup.enter="submit" :error="$v.form.email.$error" />
+      <q-btn color="primary" margin @click="submit" style="margin:10px">Submit</q-btn>
+    </div>
+  </q-page>
 </template>
+
 <script>
+import {required, email} from 'vuelidate/lib/validators'
 export default {
-  name: 'RegisterFrom',
+  data() {
+    return {
+      form: {
+        username: '',
+        password: '',
+        passwordConfirm: '',
+        FullName: '',
+        Phone: '',
+        email: '',
+      },
+    }
+  },
+  validations: {
+    form: {
+      email: {required, email},
+      username: {required},
+      password: {required},
+      passwordConfirm: {required},
+    },
+  },
+  methods: {
+    submit() {
+      this.$v.form.$touch()
+
+      if (this.$v.form.$error) {
+        this.$q.notify('Please review fields again.')
+        return 0
+      }
+    },
+  },
 }
 </script>
-<style>
-</style>
