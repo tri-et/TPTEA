5948f916bce9efd36e027584ee59ea0f168918e7
Unknown <tripham178@gmail.com>
2018-08-30 10:26:17 +0700

    change component member

---
+++ src/components/news.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-carousel class="component-top" style="max-width: 700px;margin:auto; height:300px">
+    <q-carousel-slide img-src="assets/component.jpg">
+      <div class="absolute-top-left custom-caption-top">
+        <div class="q-display-1" bg-color="brown-1">Bring on the fruit</div>
+        <q-btn class="fixed-bottom-left q-ml-sm q-mb-sm" color="white" text-color="brown" rounded>
+          <q-item @click.native="show">
+            <q-item-main label="test" />
+          </q-item>
+        </q-btn>
+        <slot></slot>
+      </div>
+    </q-carousel-slide>
+  </q-carousel>
+</template>
+<script>
+export default {
+  methods: {
+    show() {
+      this.$slots.default[0].componentInstance.opened = true
+    },
+  },
+}
+</script>
+<style>
+</style>
--- src/pages/order.vue
+++ src/pages/order.vue
@@ -1,25 +1,27 @@
 <template>
-<div>
-    <!-- popup modal -->
-      <q-modal-layout>
-        <q-scroll-observable @scroll="userHasScrolled" />
-        <div id="labels" class=" transbox">
-          <q-btn flat icon="close" @click="basicModal = false">
-            <div style="margin-left: 10px; text-transform: initial;">Bring on the fruit</div>
-          </q-btn>
-        </div>
-         <img src="assets/header-pop.jpg" alt="ww" style="width: 683px; height:300px">
-        <q-btn flat icon="close" class="absolute-top-left" @click="basicModal = false" style="color: white;"/>
-        <div style="padding: 50px">
-          <div class="q-display-1 q-mb-md">Basic Modal</div>
-          <p v-for="n in 15" :key="`a-${n}`">Scroll down to close</p>
-          <q-btn to="/orders" class="Order" rounded>
-            <div>Order</div>
-          </q-btn>
-        </div>
-      </q-modal-layout>
-  <!-- popup modal -->
-</div>
+  <q-page padding class="row justify-center">
+    <div style="width: 500px; max-width: 90vw;">
+      <p class="caption">
+        Scroll down
+        <q-icon style="font-size: 3rem" class="animate-bounce" name="keyboard_arrow_down" />
+      </p>
+      <p class="caption">
+        A circular button is set to appear after scrolling 500px.
+        <br> After another 500px another element will appear on the left.
+      </p>
+      <q-page-sticky position="bottom-right" :offset="[18, 18]">
+        <q-btn color="primary" fab v-back-to-top.animate="{offset: 500, duration: 200}" class="animate-pop">
+          <q-icon name="keyboard_arrow_up" />
+        </q-btn>
+      </q-page-sticky>
+      <q-page-sticky position="top-left" :offset="[0, 100]">
+        <a v-back-to-top.animate="1000" class="animate-pop play-backtotop non-selectable shadow-2" v-ripple.mat>
+          Back to top
+        </a>
+      </q-page-sticky>
+      <p class="caption" v-for="n in 50" :key="n">Keep scrolling...</p>
+    </div>
+  </q-page>
 </template>
 <script>
 export default {
