945f670afd9e6d1c12e0fd0a0114b5c18885f06f
Se7en-et <alvin.elitetech@gmail.com>
2018-10-10 15:18:42 +0700

    Create table user admin (#21)

---
+++ server/models/admin.js
@@ -0,0 +1,16 @@
+'use strict'
+module.exports = (sequelize, DataTypes) => {
+  var Admin = sequelize.define(
+    'Admin',
+    {
+      username: DataTypes.STRING,
+      password: DataTypes.STRING,
+      roles: DataTypes.STRING,
+    },
+    {}
+  )
+  Admin.associate = function(models) {
+    // associations can be defined here
+  }
+  return Admin
+}
