e199e74ae8a876eba13b6ec0044f73a99dbf6b09
Unknown <tripham178@gmail.com>
2018-10-12 13:57:01 +0700

    modify _auth and create _authAdmin function

--- server/util.js
+++ server/util.js
@@ -2,4 +2,16 @@ export const _auth = authCustomer => {
   if (!authCustomer) {
     throw new Error('Please login first!')
   }
+  if (authCustomer.roles) {
+    throw new Error('Account not customer!')
+  }
+}
+
+export const _authAdmin = authAdmin => {
+  if (!authAdmin) {
+    throw new Error('Please login first!')
+  }
+  if (authAdmin.roles == undefined) {
+    throw new Error('Account not admin!')
+  }
 }
