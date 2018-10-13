0fa3440f7dfccf9398916923f76b9a2564165934
tri-et <tripham178@gmail.com>
2018-10-13 14:07:43 +0700

    server _auth and _authAdmin validation (#26)
    * create page menu detail
    
    * load dynamic image header menudetail
    
    * changes requested
    
    * changes requested
    
    /statics/ -> statics/
    async listModifies -> fetchModifiers
    
    * change value default for counter
    
    * add link fix bug into changelog
    
    * add button register and login facebook
    
    * Changes requested
    
    * Changes requested
    
    * create a reusable async function
    
    * remove file mutation
    
    * Changes requested
    
    * change interface register page
    
    * remove register facebook in login page
    
    * change color "Or" word in register page
    
    * show customer's information after login
    
    * changes requested
    
    * sign out admin
    
    * create schema admin
    
    * add des into changelog
    
    * change text error msg
    
    * modify _auth and create _authAdmin function
    
    * change requested

--- server/util.js
+++ server/util.js
@@ -2,4 +2,16 @@ export const _auth = authCustomer => {
   if (!authCustomer) {
     throw new Error('Please login first!')
   }
+  if (authCustomer.roles) {
+    throw new Error('This is not a Valid Customer account!')
+  }
+}
+
+export const _authAdmin = authAdmin => {
+  if (!authAdmin) {
+    throw new Error('Please login as an Admin first!')
+  }
+  if (authAdmin.roles == undefined) {
+    throw new Error('This Account is not an Admin!')
+  }
 }
