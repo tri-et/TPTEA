df02601928f100c982e81dcbdf84ce9b05374ddd
rooneyet <42233549+rooneyet@users.noreply.github.com>
2018-10-19 18:11:07 +0700

    function getAdmin  (#32)
    * function getAdmin
    
    * change getAdmin to fetchAdmin
    
    * update changelog
    
    * change file changelog.md

--- src/pages/admin/Home.vue
+++ src/pages/admin/Home.vue
@@ -1,9 +1,21 @@
 <template>
   <div>
-    <h4 class="q-ma-xl">Welcome to TP-TEA Admin Page</h4>
+    <h4 class="q-ma-xl">Welcome {{getAdmin.username}} to TP-TEA Admin Page</h4>
   </div>
 </template>
 
 <script>
-export default {}
+import {mapGetters, mapActions} from 'vuex'
+export default {
+  name: 'PageAdmin',
+  computed: {
+    ...mapGetters('admin', ['getAdmin']),
+  },
+  methods: {
+    ...mapActions('admin', ['fetchAdmin']),
+  },
+  mounted() {
+    this.fetchAdmin()
+  },
+}
 </script>
