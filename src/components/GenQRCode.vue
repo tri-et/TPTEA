e249bdcd66b6f37071351e694d02493e53ac2a74
Unknown <tripham178@gmail.com>
2018-11-09 11:47:01 +0700

    changes requested

--- src/components/GenQRCode.vue
+++ src/components/GenQRCode.vue
@@ -20,10 +20,7 @@ export default {
       type: String,
       default: () => '#0000',
     },
-    version: {
-      type: Number,
-      default: () => 6,
-    },
+    version: 6,
   },
   methods: {
     drawImage(qrcode) {
