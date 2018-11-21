0ba183ca8f29b3eaf8f0058cde9c7a742303bb03
hemlk <truonglehiep@gmail.com>
2018-11-21 12:53:36 +0000

    0.0.2u

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.a21ffb36c90487286e49f669333823cf.js"
+  "precache-manifest.83baba5c6ee0acdde1150712fa0fde54.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "tptea"});
