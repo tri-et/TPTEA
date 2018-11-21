4ad67583d3751a32bb2db283ac19a7a3c5762ae9
hemlk <truonglehiep@gmail.com>
2018-11-21 12:20:48 +0000

    0.0.2t

--- dist/pwa-mat/precache-manifest.a21ffb36c90487286e49f669333823cf.js
+++ dist/pwa-mat/precache-manifest.a21ffb36c90487286e49f669333823cf.js
@@ -4,7 +4,7 @@ self.__precacheManifest = [
     "url": "statics/icons/icon-256x256.png"
   },
   {
-    "revision": "8e3c73bac26ce9f4cbdf791ee64020dd",
+    "revision": "28bd34e5715c6d432efcb230e7d626b6",
     "url": "index.html"
   },
   {
@@ -24,10 +24,6 @@ self.__precacheManifest = [
     "url": "statics/splash/ipadpro2_splash.png"
   },
   {
-    "revision": "5ca0bb2397976925b63560b620c8fddd",
-    "url": "statics/splash/ipadpro3_splash.png"
-  },
-  {
     "revision": "e0e41797ce02ad7afa59302519f65062",
     "url": "statics/splash/iphonexsmax_splash.png"
   },
@@ -36,8 +32,12 @@ self.__precacheManifest = [
     "url": "statics/splash/ipadpro1_splash.png"
   },
   {
-    "revision": "4f11d96ddf31938e9cdb732d7c15e151",
-    "url": "statics/splash/ipad_splash.png"
+    "revision": "5ca0bb2397976925b63560b620c8fddd",
+    "url": "statics/splash/ipadpro3_splash.png"
+  },
+  {
+    "revision": "9f28eba222b46312144302066bcf5445",
+    "url": "statics/splash/iphoneplus_splash.png"
   },
   {
     "revision": "791d460848051485414e037bfbcf3452",
@@ -48,28 +48,32 @@ self.__precacheManifest = [
     "url": "statics/category-05.jpg"
   },
   {
-    "revision": "c285996d6f7dbef24ebbbfbe630e812a",
-    "url": "statics/icons/china-flag.png"
+    "revision": "94927b05c26b2a1b4d585c7305b9d04d",
+    "url": "statics/icons/apple-icon-152x152.png"
   },
   {
-    "revision": "7d658a65f98809eb34686beb06e127d3",
-    "url": "statics/icons/icon-128x128.png"
+    "revision": "c285996d6f7dbef24ebbbfbe630e812a",
+    "url": "statics/icons/china-flag.png"
   },
   {
     "revision": "e7d738849c406fb0708b6e212dabf9a2",
     "url": "statics/icons/favicon-16x16.png"
   },
   {
-    "revision": "94927b05c26b2a1b4d585c7305b9d04d",
-    "url": "statics/icons/apple-icon-152x152.png"
+    "revision": "7d658a65f98809eb34686beb06e127d3",
+    "url": "statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "943b239ce0d615228200f7190de047d8",
+    "url": "statics/icons/favicon-32x32.png"
   },
   {
     "revision": "77bce50393376332bc7b948a33f1143f",
     "url": "statics/icons/icon-192x192.png"
   },
   {
-    "revision": "943b239ce0d615228200f7190de047d8",
-    "url": "statics/icons/favicon-32x32.png"
+    "revision": "262f954ddadfae773a3548b2330ea819",
+    "url": "statics/icons/taiwan-flag.png"
   },
   {
     "revision": "c3033218b49491bb0ac4cc87d10c8ed8",
@@ -84,10 +88,6 @@ self.__precacheManifest = [
     "url": "statics/qr-code-sample.png"
   },
   {
-    "revision": "262f954ddadfae773a3548b2330ea819",
-    "url": "statics/icons/taiwan-flag.png"
-  },
-  {
     "revision": "9be51b3fc732ce5f5931631c5fe6a42e",
     "url": "statics/category-07.jpg"
   },
@@ -96,8 +96,8 @@ self.__precacheManifest = [
     "url": "statics/icons/hongkong-flag.png"
   },
   {
-    "revision": "34a10bda72f60c31ca0c",
-    "url": "js/app.dadcab9.js"
+    "revision": "26ff84e3f1e2d028490b",
+    "url": "js/app.638709a.js"
   },
   {
     "revision": "33fa3c71f424a8e2cbc06ddeadce0779",
@@ -108,12 +108,12 @@ self.__precacheManifest = [
     "url": "statics/icons/icon-512x512.png"
   },
   {
-    "revision": "a54d4cff09320803d08dc3c14eb436ad",
-    "url": "statics/category-01.jpg"
-  },
-  {
     "revision": "c38f90d42db60f76c44a13d9ca345b04",
     "url": "statics/category-02.jpg"
+  },
+  {
+    "revision": "a54d4cff09320803d08dc3c14eb436ad",
+    "url": "statics/category-01.jpg"
   },
   {
     "revision": "b3b5cd863f68ad9e430a1df56904a8d2",
@@ -148,12 +148,12 @@ self.__precacheManifest = [
     "url": "statics/splash/iphonexr_splash.png"
   },
   {
-    "revision": "9f28eba222b46312144302066bcf5445",
-    "url": "statics/splash/iphoneplus_splash.png"
-  },
-  {
     "revision": "cdec18eac0fb4339eba6bdeff36f75c4",
     "url": "statics/splash/iphonex_splash.png"
+  },
+  {
+    "revision": "4f11d96ddf31938e9cdb732d7c15e151",
+    "url": "statics/splash/ipad_splash.png"
   },
   {
     "revision": "d9c608dcea837a8f2c275b0b874da757",
@@ -192,7 +192,7 @@ self.__precacheManifest = [
     "url": "fonts/KFOkCnqEu92Fr1MmgVxIIzQ.e9dbbe8.woff"
   },
   {
-    "revision": "34a10bda72f60c31ca0c",
-    "url": "css/app.0e77feaa.css"
+    "revision": "26ff84e3f1e2d028490b",
+    "url": "css/app.99411fcc.css"
   }
 ];
--- dist/pwa-mat/service-worker.js
+++ dist/pwa-mat/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.6c5da3641bb257f4e361bc2b259e4231.js"
+  "precache-manifest.a21ffb36c90487286e49f669333823cf.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "tptea"});
