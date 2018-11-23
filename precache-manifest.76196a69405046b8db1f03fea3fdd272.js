e9bbed951f1d189300211b9f00948d86bfd590dc
John-et <tripham178@gmail.com>
2018-11-23 10:45:12 -0800

    test

--- precache-manifest.76196a69405046b8db1f03fea3fdd272.js
+++ precache-manifest.76196a69405046b8db1f03fea3fdd272.js
@@ -32,12 +32,12 @@ self.__precacheManifest = [
     "url": "/statics/splash/ipadpro1_splash.png"
   },
   {
-    "revision": "cdec18eac0fb4339eba6bdeff36f75c4",
-    "url": "/statics/splash/iphonex_splash.png"
-  },
-  {
     "revision": "e0e41797ce02ad7afa59302519f65062",
     "url": "/statics/splash/iphonexsmax_splash.png"
+  },
+  {
+    "revision": "cdec18eac0fb4339eba6bdeff36f75c4",
+    "url": "/statics/splash/iphonex_splash.png"
   },
   {
     "revision": "4f11d96ddf31938e9cdb732d7c15e151",
@@ -48,24 +48,24 @@ self.__precacheManifest = [
     "url": "/statics/news1.jpg"
   },
   {
-    "revision": "94927b05c26b2a1b4d585c7305b9d04d",
-    "url": "/statics/icons/apple-icon-152x152.png"
-  },
-  {
-    "revision": "943b239ce0d615228200f7190de047d8",
-    "url": "/statics/icons/favicon-32x32.png"
+    "revision": "e7d738849c406fb0708b6e212dabf9a2",
+    "url": "/statics/icons/favicon-16x16.png"
   },
   {
     "revision": "c285996d6f7dbef24ebbbfbe630e812a",
     "url": "/statics/icons/china-flag.png"
   },
   {
-    "revision": "e7d738849c406fb0708b6e212dabf9a2",
-    "url": "/statics/icons/favicon-16x16.png"
-  },
-  {
     "revision": "77bce50393376332bc7b948a33f1143f",
     "url": "/statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "94927b05c26b2a1b4d585c7305b9d04d",
+    "url": "/statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "943b239ce0d615228200f7190de047d8",
+    "url": "/statics/icons/favicon-32x32.png"
   },
   {
     "revision": "7d658a65f98809eb34686beb06e127d3",
@@ -76,16 +76,16 @@ self.__precacheManifest = [
     "url": "/statics/icons/ms-icon-144x144.png"
   },
   {
+    "revision": "262f954ddadfae773a3548b2330ea819",
+    "url": "/statics/icons/taiwan-flag.png"
+  },
+  {
     "revision": "6af85f4c91ff8304e9448667906a39c4",
     "url": "/statics/icons/tptea-marker-icon.png"
   },
   {
     "revision": "644d672e528a3a2c937bb8156ee55e73",
     "url": "/statics/qr-code-sample.png"
-  },
-  {
-    "revision": "262f954ddadfae773a3548b2330ea819",
-    "url": "/statics/icons/taiwan-flag.png"
   },
   {
     "revision": "9be51b3fc732ce5f5931631c5fe6a42e",
@@ -100,12 +100,12 @@ self.__precacheManifest = [
     "url": "/js/app.7d28c2f.js"
   },
   {
-    "revision": "c2b35452f36dcfc83a03c07a8ead6104",
-    "url": "/statics/icons/icon-512x512.png"
-  },
-  {
     "revision": "33fa3c71f424a8e2cbc06ddeadce0779",
     "url": "/statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "c2b35452f36dcfc83a03c07a8ead6104",
+    "url": "/statics/icons/icon-512x512.png"
   },
   {
     "revision": "a54d4cff09320803d08dc3c14eb436ad",
