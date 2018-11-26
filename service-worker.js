0b7921711ba107f482e1c693a5eda04d3f5ab6c8
John-et <tripham178@gmail.com>
2018-11-26 13:39:40 +0700

    testing

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,32 @@
-importScripts("/precache-manifest.85927fcb05d8bdcbdc291662fb189aba.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js");
+importScripts("/precache-manifest.37f4d1e51c76cd4780e7898bf16d6465.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js");
 
 /*
  * This file (which will be your service worker)
  * is picked up by the build system ONLY if
  * quasar.conf > pwa > workboxPluginMode is set to "InjectManifest"
  */
-const CACHE_NAME = 'V599'
+const CACHE_NAME = 'V1'
 workbox.routing.registerRoute(
   new RegExp('/'),
-  workbox.strategies.cacheFirst({
+  workbox.strategies.staleWhileRevalidate({
     cacheName: CACHE_NAME,
   })
 )
 self.addEventListener('message', messageEvent => {
   if (messageEvent.data === 'skipWaiting') {
     self.skipWaiting()
-    self.clients.claim()
   }
 })
 self.addEventListener('activate', function(event) {
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(cacheNames => {
       let validCacheSet = new Set(Object.values(workbox.core.cacheNames))
       return Promise.all(
         cacheNames
-          .filter(function(cacheName) {
+          .filter(cacheName => {
             return !validCacheSet.has(cacheName)
           })
-          .map(function(cacheName) {
+          .map(cacheName => {
             return caches.delete(cacheName)
           })
       )
