75ddee4617d275747a5e205e5295e8b8f3b3bbee
John <tripham178@gmail.com>
2018-11-26 09:39:06 +0000

    test pwa

--- dist/pwa-mat/service-worker.js
+++ dist/pwa-mat/service-worker.js
@@ -1,36 +1,37 @@
-importScripts("/precache-manifest.01946934398bc23dd7413664fa4b72e7.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js");
+importScripts("/precache-manifest.1328c102be478c570ee8faa7eb8fd297.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js");
 
 /*
  * This file (which will be your service worker)
  * is picked up by the build system ONLY if
  * quasar.conf > pwa > workboxPluginMode is set to "InjectManifest"
  */
-const CACHE_NAME = 'V_3'
-workbox.routing.registerRoute(
-  new RegExp('/'),
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: CACHE_NAME,
-  })
-)
-self.addEventListener('message', messageEvent => {
-  if (messageEvent.data === 'skipWaiting') {
-    self.skipWaiting()
-  }
-})
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      let validCacheSet = new Set(Object.values(workbox.core.cacheNames))
-      return Promise.all(
-        cacheNames
-          .filter(cacheName => {
-            return !validCacheSet.has(cacheName)
-          })
-          .map(cacheName => {
-            return caches.delete(cacheName)
-          })
-      )
-    })
-  )
-})
+// const CACHE_NAME = 'V_3'
+self.workbox.precaching.precache(self.__precacheManifest)
+// workbox.routing.registerRoute(
+//   new RegExp('/'),
+//   workbox.strategies.staleWhileRevalidate({
+//     cacheName: CACHE_NAME,
+//   })
+// )
+// self.addEventListener('message', messageEvent => {
+//   if (messageEvent.data === 'skipWaiting') {
+//     self.skipWaiting()
+//   }
+// })
+// self.addEventListener('activate', function(event) {
+//   event.waitUntil(
+//     caches.keys().then(cacheNames => {
+//       let validCacheSet = new Set(Object.values(workbox.core.cacheNames))
+//       return Promise.all(
+//         cacheNames
+//           .filter(cacheName => {
+//             return !validCacheSet.has(cacheName)
+//           })
+//           .map(cacheName => {
+//             return caches.delete(cacheName)
+//           })
+//       )
+//     })
+//   )
+// })
 
--- src-pwa/custom-service-worker.js
+++ src-pwa/custom-service-worker.js
@@ -3,31 +3,32 @@
  * is picked up by the build system ONLY if
  * quasar.conf > pwa > workboxPluginMode is set to "InjectManifest"
  */
-const CACHE_NAME = 'V_3'
-workbox.routing.registerRoute(
-  new RegExp('/'),
-  workbox.strategies.staleWhileRevalidate({
-    cacheName: CACHE_NAME,
-  })
-)
-self.addEventListener('message', messageEvent => {
-  if (messageEvent.data === 'skipWaiting') {
-    self.skipWaiting()
-  }
-})
-self.addEventListener('activate', function(event) {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      let validCacheSet = new Set(Object.values(workbox.core.cacheNames))
-      return Promise.all(
-        cacheNames
-          .filter(cacheName => {
-            return !validCacheSet.has(cacheName)
-          })
-          .map(cacheName => {
-            return caches.delete(cacheName)
-          })
-      )
-    })
-  )
-})
+// const CACHE_NAME = 'V_3'
+self.workbox.precaching.precache(self.__precacheManifest)
+// workbox.routing.registerRoute(
+//   new RegExp('/'),
+//   workbox.strategies.staleWhileRevalidate({
+//     cacheName: CACHE_NAME,
+//   })
+// )
+// self.addEventListener('message', messageEvent => {
+//   if (messageEvent.data === 'skipWaiting') {
+//     self.skipWaiting()
+//   }
+// })
+// self.addEventListener('activate', function(event) {
+//   event.waitUntil(
+//     caches.keys().then(cacheNames => {
+//       let validCacheSet = new Set(Object.values(workbox.core.cacheNames))
+//       return Promise.all(
+//         cacheNames
+//           .filter(cacheName => {
+//             return !validCacheSet.has(cacheName)
+//           })
+//           .map(cacheName => {
+//             return caches.delete(cacheName)
+//           })
+//       )
+//     })
+//   )
+// })
