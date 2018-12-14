9947e5a9d126082056010eddebe0dcde2b63a5af
John-et <tripham178@gmail.com>
2018-12-14 20:20:14 +0700

    push message

--- quasar.conf.js
+++ quasar.conf.js
@@ -30,7 +30,7 @@ module.exports = function(ctx) {
       },
     },
     devServer: {
-      https: false,
+      https: true,
       // port: 8080,
       open: true, // opens browser window automatically
       proxy: {
