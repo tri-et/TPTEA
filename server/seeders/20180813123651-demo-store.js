f52e2d71a194e7977ce18a5f3d946436130e3429
dp <phillip7.et@gmail.com>
2019-01-10 18:09:50 +0700

    Create, seed Stores & update Orders table (#114)
    * add migration add column to table order
    
    * add transaction into migration
    
    The difference of adding  transaction : https://prnt.sc/m56wpy
    
    * create stores table, modal, foreign key, relationship
    
    * fix migration and seed store data

--- server/seeders/20180813123651-demo-store.js
+++ server/seeders/20180813123651-demo-store.js
@@ -3,28 +3,35 @@
 var GoogleSpreadsheet = require('google-spreadsheet')
 var _d = require('lodash')
 var creds = require('../TP-TEA-HK-4be78b7ad5f8.json')
-var doc = new GoogleSpreadsheet('1jjROzUPDDOz5MgRLlG6uyA3SJz7ps5fmw-L6SI81gD8')
+
+// Create a document object using the ID of the spreadsheet - obtained from its URL.
+var doc = new GoogleSpreadsheet('1qUFzIif4CFQ64-jf4OOtFQK8rKY7YHqN3PxSATHjy3w')
+
 function getData() {
   return new Promise((resolve, reject) => {
-    doc.useServiceAccountAuth(creds, function(err) {
-      doc.getRows(1, function(err, rows) {
+    doc.useServiceAccountAuth(creds, err => {
+      doc.getRows(1, (err, rows) => {
         if (err) reject(err)
         else resolve(rows)
       })
     })
   })
 }
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     var data = await getData().catch(err => console.log(err))
+    // console.log(data)
     return queryInterface.bulkInsert(
-      'orders',
-      _d.map(data, row => _d.pick(row, 'id', 'customerid')),
+      'stores',
+      _d.map(data, row =>
+        _d.pick(row, ['id', 'name', 'address', 'phone', 'lat', 'lng', 'city', 'nation'])
+      ),
       {}
     )
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkDelete('orders', null, {})
+    return queryInterface.bulkDelete('stores', null, {})
   },
 }
--- server/seeders/20190110123653-demo-order.js
+++ server/seeders/20190110123653-demo-order.js
@@ -1,31 +1,30 @@
 'use strict'
+
 var GoogleSpreadsheet = require('google-spreadsheet')
 var _d = require('lodash')
 var creds = require('../TP-TEA-HK-4be78b7ad5f8.json')
-var doc = new GoogleSpreadsheet('1GMxPRlBSyukoiGSETq7xU4p0UsOv6zUNr0HyMO0MxoQ')
-
+var doc = new GoogleSpreadsheet('1jjROzUPDDOz5MgRLlG6uyA3SJz7ps5fmw-L6SI81gD8')
 function getData() {
   return new Promise((resolve, reject) => {
-    doc.useServiceAccountAuth(creds, function(err) {
-      doc.getRows(1, function(err, rows) {
+    doc.useServiceAccountAuth(creds, function (err) {
+      doc.getRows(1, function (err, rows) {
         if (err) reject(err)
         else resolve(rows)
       })
     })
   })
 }
-
 module.exports = {
   async up(queryInterface, Sequelize) {
     var data = await getData().catch(err => console.log(err))
     return queryInterface.bulkInsert(
-      'orderdetails',
-      _d.map(data, row => _d.pick(row, 'id', 'orderid', 'menuid', 'quantity', 'price')),
+      'orders',
+      _d.map(data, row => _d.pick(row, 'id', 'customerid','storeid','delivery-address')),
       {}
     )
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('orderdetails', null, {})
+    return queryInterface.bulkDelete('orders', null, {})
   },
 }
