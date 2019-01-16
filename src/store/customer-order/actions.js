5ca903f5b5b4838bdd677b7981be9c44f2d937b9
John-et <tripham178@gmail.com>
2019-01-16 14:31:06 +0700

    set customerId in placeOrder

--- src/store/customer-order/actions.js
+++ src/store/customer-order/actions.js
@@ -1,10 +1,14 @@
 import {_post, _procError, _procAlert, _alert} from '../../util/common'
+import store from '../index'
+import _d from 'lodash'
 export const placeOrder = ({commit, getters}) => {
-  if (getters.getRecs.customerId) {
+  let customer = store().getters['customer/getCustomer']
+  if (!_d.isEmpty(customer)) {
     commit('setIsLoading', true)
     _post(
       {
         ...getters.getRecs,
+        customerId: customer.id,
         placeOrderMethod: getters.getPlaceOrderMethod,
       },
       `mutation ($input: OrderInput) {
